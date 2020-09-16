82e2c2497be56818cab34be69756857a76283b43
claire-labry <clairestjlabry@gmail.com>
2020-09-16 12:56:53 -0500

    wrapping up on tasks routes

--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -112,13 +112,13 @@ router.delete('/:id', auth, async(req, res) =>{
 //  @route   PUT api/tasks/reminder/:id
 //  @desc    add a reminder to your task
 //  @access  private
-router.get('/reminder/:id', auth, async(req, res)=>{
+router.put('/reminder/:id', auth, async(req, res)=>{
 try {
     const task = await Task.findById(req.params.id);
 
     // check if your task already has a reminder
     if(task.reminders.filter(reminder => reminder.user.toString() === req.user.id).length > 0){
-        return res.json(400).json({msg: '~your task already has a reminder~'});
+        return res.status(400).json({msg: '~your task already has a reminder~'});
     }
     task.reminders.unshift({user: req.user.id});
 
@@ -130,6 +130,108 @@ try {
     console.error(err.message);
     res.status(500).send('~server error~');
 }
-})
+});
+
+//  @route   PUT api/tasks/unremind/:id
+//  @desc    remove your reminder
+//  @access  private
+router.put('/unremind/:id', auth, async(req, res)=>{
+try {
+    const task = await Task.findById(req.params.id);
+
+    // check if your task already has a reminder
+    if(task.reminders.filter(reminder => reminder.user.toString() === req.user.id).length === 0){
+        return res.status(400).json({msg: '~reminder removed~'});
+    }
+    
+    // get remove index
+    const removeIndex = task.reminders.map(reminder => reminder.user.toString()).indexOf(req.user.id);
+
+    task.reminders.splice(removeIndex, 1)
+
+    await task.save();
+
+    res.json(task.reminders)
+
+} catch (err) {
+    console.error(err.message);
+    res.status(500).send('~server error~');
+}
+});
+
+//  @route   POST api/tasks/comment/:id
+//  @desc    add a comment to your task
+//  @access  private
+router.post(
+    '/comment/:id',
+    [
+        auth, 
+        [
+            check('text', '~text is required~')
+            .not()
+            .isEmpty()
+            ]
+        ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({ errors:errors.array() });
+        }
+    
+        try {
+            const user = await User.findById(req.user.id).select('-password');
+
+            const task = await Task.findById(req.params.id);
+        
+            const newComment = new Task ({
+                text: req.body.text,
+                name: user.name,
+                avatar: user.avatar,
+                user: req.user.id
+            });
+
+            task.comments.unshift(newComment);
+
+            await task.save();
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('~server error~')
+        }
+    },
+    );
+
+//  @route   DELETE api/tasks/comment/:id
+//  @desc    add a comment to your task
+//  @access  private
+router.delete(
+    '/comment/:id', auth, async (req, res) => {
+    
+        try {
+            const task = await Task.findById(req.params.id);
+        
+            const comment = task.comments.find(comment => comment.id === req.params.comment_id);
+
+            // ensures that the comment does exist
+            if(!comment) {
+                return res.status(404).json({ msg: '~comment does not exist~' });
+            }
+
+            // double check the user
+            if(comment.user.toString() !== req.user.id){
+                return res.status(401).json({msg:'~user is not authorized~'});
+            }
+
+            // remove index
+            const removeIndex = task.comments.map(comment => comment.id).indexOf(req.params.comment_id);
+
+            task.comments.splice(removeIndex, 1)
+            await task.save();
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('~server error~')
+        }
+    },
+    );
+
 
 module.exports = router;
